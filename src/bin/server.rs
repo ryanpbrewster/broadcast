585668a03b91f05dcd4c2245791ba6af3b09715b
Ryan P. Brewster <RyanPBrewster@gmail.com>
2017-10-07 15:20:07 -0700

    Minor prep for streaming output

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,17 +1,10 @@
 extern crate futures;
-use futures::Stream;
 
 extern crate grpc;
-
-extern crate protobuf;
-use protobuf::repeated::RepeatedField;
 
 extern crate broadcast;
 
 use std::thread;
-use std::iter::FromIterator;
-use std::sync::{Arc, RwLock};
-use std::collections::HashSet;
 
 use broadcast::proto::service_grpc::{Broadcast, BroadcastServer};
 use broadcast::proto::service::{BroadcastRequest, BroadcastReply, ListenRequest, ListenEvent};
@@ -37,7 +30,7 @@ impl Broadcast for BroadcastImpl {
 
         let mut pong = ListenEvent::new();
         pong.set_msg("pong".to_owned());
-        grpc::StreamingResponse::completed(vec![pong])
+        grpc::StreamingResponse::no_metadata(futures::stream::once(Ok(pong)))
     }
 }
 
@@ -45,10 +38,12 @@ const PORT: u16 = 50051;
 fn main() {
     let mut server = grpc::ServerBuilder::new_plain();
     server.http.set_port(PORT);
-    server.add_service(BroadcastServer::new_service_def(BroadcastImpl));
     server.http.set_cpu_pool_threads(4);
-    let _server = server.build().expect("server");
 
+    let broadcaster = BroadcastImpl;
+    server.add_service(BroadcastServer::new_service_def(broadcaster));
+
+    let _server = server.build().expect("server");
     println!("server listening at port {}", PORT);
     loop {
         thread::park();
