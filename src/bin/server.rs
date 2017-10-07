a1ddfb5c16b2ecf9cda424a0eb89e20d6102ccb7
Ryan P. Brewster <RyanPBrewster@gmail.com>
2017-10-07 15:08:42 -0700

    Simple listen client, currently broken

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -14,24 +14,34 @@ use std::sync::{Arc, RwLock};
 use std::collections::HashSet;
 
 use broadcast::proto::service_grpc::{Broadcast, BroadcastServer};
-use broadcast::proto::service::{BroadcastRequest, BroadcastReply};
+use broadcast::proto::service::{BroadcastRequest, BroadcastReply, ListenRequest, ListenEvent};
 
 struct BroadcastImpl;
 
 impl Broadcast for BroadcastImpl {
-    fn broadcast(&self, _m: grpc::RequestOptions, req: BroadcastRequest) -> grpc::SingleResponse<BroadcastReply> {
+    fn broadcast(&self, _opts: grpc::RequestOptions, req: BroadcastRequest) -> grpc::SingleResponse<BroadcastReply> {
         println!("broadcasting {}", req.msg);
         grpc::SingleResponse::completed(BroadcastReply::new())
     }
+
+    fn listen(&self, _opts: grpc::RequestOptions, _req: ListenRequest) -> grpc::StreamingResponse<ListenEvent> {
+        println!("listening...");
+
+        let mut pong = ListenEvent::new();
+        pong.set_msg("pong".to_owned());
+        grpc::StreamingResponse::completed(vec![pong])
+    }
 }
 
+const PORT: u16 = 50051;
 fn main() {
     let mut server = grpc::ServerBuilder::new_plain();
-    server.http.set_port(50051);
+    server.http.set_port(PORT);
     server.add_service(BroadcastServer::new_service_def(BroadcastImpl));
     server.http.set_cpu_pool_threads(4);
     let _server = server.build().expect("server");
 
+    println!("server listening at port {}", PORT);
     loop {
         thread::park();
     }
