b77920186cc5affdf70f52e98759d6841166231d
Ryan P. Brewster <RyanPBrewster@gmail.com>
2017-10-07 17:41:47 -0700

    Broadcasting done

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,16 +1,20 @@
 extern crate futures;
 use futures::{Future, Sink, Stream};
+use futures::sync::mpsc::UnboundedSender;
 
 extern crate grpc;
 
 extern crate broadcast;
 
 use std::thread;
+use std::sync::{Arc, RwLock};
 
 use broadcast::proto::service_grpc::{Broadcast, BroadcastServer};
 use broadcast::proto::service::{BroadcastRequest, BroadcastReply, ListenRequest, ListenEvent};
 
-struct BroadcastImpl;
+struct BroadcastImpl {
+    receivers: Arc<RwLock<Vec<UnboundedSender<String>>>>,
+}
 
 impl Broadcast for BroadcastImpl {
     fn broadcast(
@@ -19,6 +23,14 @@ impl Broadcast for BroadcastImpl {
         req: BroadcastRequest,
     ) -> grpc::SingleResponse<BroadcastReply> {
         println!("broadcasting {}", req.msg);
+        {
+            let listeners = self.receivers.read().expect(
+                "acquiring write-lock for receivers",
+            );
+            for tx in listeners.iter() {
+                tx.send(req.msg.clone());
+            }
+        }
         grpc::SingleResponse::completed(BroadcastReply::new())
     }
 
@@ -29,7 +41,13 @@ impl Broadcast for BroadcastImpl {
     ) -> grpc::StreamingResponse<ListenEvent> {
         println!("listening...");
 
-        let (tx, rx) = futures::sync::mpsc::channel::<String>(1);
+        let (tx, rx) = futures::sync::mpsc::unbounded::<String>();
+        {
+            self.receivers
+                .write()
+                .expect("acquiring write-lock for receivers")
+                .push(tx);
+        }
         let mut pongs = rx.map(|msg: String| {
             let mut pong = ListenEvent::new();
             pong.set_msg(msg);
@@ -37,7 +55,6 @@ impl Broadcast for BroadcastImpl {
         }).map_err(|err| {
                 grpc::Error::Other("TODO(rpb): add better error message")
             });
-        tx.send("asdf".to_owned()).poll();
         grpc::StreamingResponse::no_metadata(pongs)
     }
 }
@@ -48,7 +65,7 @@ fn main() {
     server.http.set_port(PORT);
     server.http.set_cpu_pool_threads(4);
 
-    let broadcaster = BroadcastImpl;
+    let broadcaster = BroadcastImpl { receivers: Arc::new(RwLock::new(Vec::new())) };
     server.add_service(BroadcastServer::new_service_def(broadcaster));
 
     let _server = server.build().expect("server");
